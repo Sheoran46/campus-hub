d4fc58f8333fcd254d0626bfe007103a2ea259d4
Sheoran46 <88sheoran@gmail.com>
2026-06-13 16:10:33 +0530

    email issue

--- src/main/java/com/sheoran/LostAndFound/LostAndFoundApplication.java
+++ src/main/java/com/sheoran/LostAndFound/LostAndFoundApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableAsync // Enable background processing for emails and AI tasks
 public class LostAndFoundApplication {
 
 	public static void main(String[] args) {
